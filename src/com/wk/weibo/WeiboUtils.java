4c7f8e5c50dfbb89b8d606340ae10a3b6b815cfc
wk <1226426603@qq.com>
2021-09-19 21:03:37 +0800

    first commit

---
+++ src/com/wk/weibo/WeiboUtils.java
@@ -0,0 +1,14 @@
+package com.wk.weibo;
+
+/**
+ * @author : wk
+ * e-mail : 1226426603@qq.com <
+ * time   : 2021/9/19
+ * desc   :
+ * <p>
+ * address:
+ * GitHub : https://github.com/wk1995
+ * CSDN   : http://blog.csdn.net/qq_33882671
+ */
+public class WeiboUtils {
+}
